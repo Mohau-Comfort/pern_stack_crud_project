4396a82356223f1b21cd67f3345474297edce6ac
Mohau Mahlakwane <mohau.mahlakwane@redmps.com>
2025-03-20 11:20:47 +0200

    updated table styling

--- client/src/components/Tablelist.jsx
+++ client/src/components/Tablelist.jsx
@@ -1,44 +1,45 @@
-//src/components/Tablelist.jsx
-
 import React from 'react';
+
+const clients = [
+  { name: "Tom Cruise", job: "Software Engineer", email: "tom@email.com" },
+  { name: "Denzel Washington", job: "Oracle DBA", email: "denzel@email.com" },
+  { name: "Stephen Curry", job: "Cyber Security", email: "chef@email.com" }
+];
 
 export const Tablelist = () => {
   return (
-    <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      {/* row 2 */}
-      <tr className="hover:bg-base-300">
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      {/* row 3 */}
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-  )
-}
+    <div className="overflow-x-auto mt-10">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Job Title</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clients.map((client, index) => (
+            <tr 
+              key={index}
+              className="hover:bg-blue-50 hover:shadow-md group transition-all duration-300"
+            >
+              <td className="group-hover:font-bold">{index + 1}</td>
+              <td className="group-hover:text-blue-600">{client.name}</td>
+              <td className="group-hover:italic">{client.job}</td>
+              <td>
+                <a 
+                  href={`mailto:${client.email}`} 
+                  className="text-gray-600 group-hover:text-blue-500 group-hover:underline"
+                >
+                  {client.email}
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
