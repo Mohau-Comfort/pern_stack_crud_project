8813721a5aea7d6df2719f701bae471c55e5056e
Mohau Mahlakwane <mohau.mahlakwane@redmps.com>
2025-03-24 11:04:58 +0200

    installed pg dependencies

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,13 @@
 //src/index.js
 import express from 'express';
+import cors from 'cors';
+import clientRoute from './routes/clientRoute.js';
 
 const app = express();
 const port = 5000; 
 
+app.use(cors()); //Enable All CORS Requests
+app.use(express.json()); //Parse incoming request with JSON payloads (Middleware)
 app.get('/', (req, res) => {
     res.send("<h1>Hello backend</h1>");
 });
